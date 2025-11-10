de31c390b918fe5db8b7d733f6610d75d07bb4c2
spy2213 <redhelbling@gmail.com>
2025-11-10 12:06:52 -0300

    Trabalhofinal1

---
+++ Trabalhofinal.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+struct paciente
+{
+    int senha;
+    string prioridade;
+    int hh;
+    int mm;
+};
+
+
+
+int main(){
+    queue<paciente> V, A, D, B;
+    char opcao;
+    while (true) {      
+        cin >> opcao;
+        switch (opcao){
+        
+        case 'C':
+            
+            break;
+        
+        default:
+            break;
+        }
+    
+    }
+
+    return 0;
+}
